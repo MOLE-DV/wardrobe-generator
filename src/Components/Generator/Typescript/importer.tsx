71d8dbbd854b98c09f7a76516560f9842c141bdd
MOLE-DV <maxx.olejnik@gmail.com>
2024-09-15 19:54:59 +0200

    Heic-disable

--- src/Components/Generator/Typescript/importer.tsx
+++ src/Components/Generator/Typescript/importer.tsx
@@ -3,20 +3,18 @@ import { useForm } from 'react-hook-form';
 import { Context } from '../generator';
 import React, { useContext, useEffect } from'react';
 
-
 export const Importer = () => {
     const { register, handleSubmit } = useForm();
     const context = useContext(Context);
 
 
     const onSubmit = (data: any) => {
-        console.log('test')
         context.setImages([...context.images,...data.images]);
     }
 
     return (
         <form id="importer" onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("images", {required: "Required"})} type="file" name="images" multiple/>
+            <input {...register("images", {required: "Required"})} type="file" name="images" multiple accept="image/png, image/gif, image/jpeg, image/jpg"/>
             <button type="submit">upload</button>
         </form>
     );
